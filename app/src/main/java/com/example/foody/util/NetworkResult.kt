1e49d7428837976686ffef3179de3e5c4bd65aa8
KyungMin <2004ckck@naver.com>
2021-04-15 21:09:16 +0900

    [CREATE] sealed Class

---
+++ app/src/main/java/com/example/foody/util/NetworkResult.kt
@@ -0,0 +1,12 @@
+package com.example.foody.util
+
+//sealed class 란 enum class 의 확장 형태이다, sealed 클래스는 클래스들을 묶은 클래스이다.
+sealed class NetworkResult<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    // 네트워크 상태에 따라 class 를 호출한다.
+    class Success<T>(data: T) : NetworkResult<T>(data)
+    class Error<T>(message: String?, data: T? = null) : NetworkResult<T>(data, message)
+    class Loading<T> : NetworkResult<T>()
+}
