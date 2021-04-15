dffa05e938bafaaacde04f6935b78e29e8716325
KyungMin <2004ckck@naver.com>
2021-04-15 21:06:22 +0900

    [CREATE] ViewModel InterNet Check

---
+++ app/src/main/java/com/example/foody/MainViewModel.kt
@@ -0,0 +1,102 @@
+package com.example.foody
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.foody.data.Repository
+import com.example.foody.models.FoodRecipe
+import com.example.foody.util.NetworkResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+// ViewModelInject 는 삭제됨 HiltViewModel 로 쓰기
+@HiltViewModel
+class MainViewModel constructor(
+    private val repository: Repository,
+    application: Application
+) : AndroidViewModel(application) {
+
+    //LiveData 선언
+    var recipesResponse: MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
+
+    fun getRecipes(queries: Map<String, String>) = viewModelScope.launch {
+
+        getRecipesSafeCall(queries)
+    }
+
+    private suspend fun getRecipesSafeCall(queries: Map<String, String>) {
+
+        // 먼저 Loading 으로
+        recipesResponse.value = NetworkResult.Loading()
+
+        //인터넷이 연결되었을 때
+        if (hasInternetConnection()) {
+            try {
+                val response = repository.remote.getRecipes(queries)
+                recipesResponse.value = handleFoodRecipeResponse(response)
+            } catch (e: Exception) {
+                recipesResponse.value = NetworkResult.Error("Recipes not found")
+            }
+        } else {
+            // 인터넷 연결 실패
+            recipesResponse.value = NetworkResult.Error("No Internet Connection.")
+        }
+    }
+
+    private fun handleFoodRecipeResponse(response: Response<FoodRecipe>): NetworkResult<FoodRecipe>? {
+        when {
+
+            //너무 오래 걸릴 때
+            response.message().toString().contains("Timeout") -> {
+                return NetworkResult.Error("Timeout")
+            }
+
+            // 402가 뜰 때
+            response.code() == 402 -> {
+                return NetworkResult.Error("API Key Limited.")
+            }
+
+            //데이터가 없을 때
+            response.body()!!.results.isNullOrEmpty() -> {
+                return NetworkResult.Error("Recipes not found.")
+            }
+
+            //성공했을 때 data 를 가져옴 
+            response.isSuccessful -> {
+                val foodRecipe = response.body()
+                return NetworkResult.Success(foodRecipe!!)
+            }
+            else -> {
+                return NetworkResult.Error(response.message())
+            }
+
+        }
+    }
+
+
+    //인터넷 확인 함수
+    @SuppressLint("WrongConstant")
+
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager = getApplication<Application>().getSystemService(
+            //연결 요청
+            Context.CONNECTIVITY_DIAGNOSTICS_SERVICE
+        ) as ConnectivityManager
+
+        //연결 관리자
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }
+}
