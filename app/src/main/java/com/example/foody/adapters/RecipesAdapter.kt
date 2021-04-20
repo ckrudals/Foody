6f613e8bbf0b5e0628a5344dfb3946854ccf2fe1
KyungMin <2004ckck@naver.com>
2021-04-20 20:37:25 +0900

    [RENAME] recipe, currentResult

--- app/src/main/java/com/example/foody/adapters/RecipesAdapter.kt
+++ app/src/main/java/com/example/foody/adapters/RecipesAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.foody.adapters
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -12,14 +14,14 @@ import com.example.foody.util.RecipesDiffUtil
 class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
 
     //dataClass 를 emptyList 로 설정
-    private var recipe = emptyList<Result>()
+    private var recipes = emptyList<Result>()
 
     class MyViewHolder(private val binding: RecpiesRowLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
-
         fun bind(result: Result) {
             binding.result = result
             // 데이터 내부에 변경 사항이있을 때마다 레이아웃을 업데이트함
+            // 바인딩을 즉시 실행할 때 -> 업데이트가 될 때 ??
             binding.executePendingBindings()
         }
 
@@ -33,24 +35,26 @@ class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        Log.d(TAG,"MyViewHolder - () called")
+
         return MyViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentResult = recipe[position]
-        holder.bind(currentResult)
+        val currentRecipe = recipes[position]
+        holder.bind(currentRecipe)
     }
 
     override fun getItemCount(): Int {
-        return recipe.size
+        return recipes.size
     }
 
     fun setData(newData: FoodRecipe) {
         // 이전꺼, 데이터가 변한값
-        val recipesDiffUtil = RecipesDiffUtil(recipe, newData.results)
+        val recipesDiffUtil = RecipesDiffUtil(recipes, newData.results)
         // 데이터의 차이?? 비교 데이터가 무엇이 변햇는지
         val diffUtilResult= DiffUtil.calculateDiff(recipesDiffUtil)
-        recipe = newData.results
+        recipes = newData.results
         //결과가 나오면 전달
         diffUtilResult.dispatchUpdatesTo(this)
     }
