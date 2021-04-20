dfcf655019e83836bab0abea942b50b24349b695
KyungMin <2004ckck@naver.com>
2021-04-20 20:38:51 +0900

    [ADD] recyclerView shimmer 효과

--- app/src/main/java/com/example/foody/ui/fragment/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragment/recipes/RecipesFragment.kt
@@ -1,22 +1,102 @@
 package com.example.foody.ui.fragment.recipes
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.foody.MainViewModel
 import com.example.foody.R
+import com.example.foody.adapters.RecipesAdapter
+import com.example.foody.util.Constrants.Companion.API_KEY
+import com.example.foody.util.NetworkResult
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
+@AndroidEntryPoint
 class RecipesFragment : Fragment() {
 
+    val TAG="RecipesFragment"
+    private lateinit var mainViewModel: MainViewModel
+    private val mAdapter by lazy { RecipesAdapter() }
+    lateinit var mView: View
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        Log.d(TAG,"RecipesFragment - onCreateView() called")
+
+        // Inflate the layout for this frag ment
+        mView = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        setupRecyclerView()
+
+        return mView
+    }
+
+    private fun requestApiData() {
+        mainViewModel.getRecipes(applyQueries())
+        mainViewModel.recipesResponse.observe(viewLifecycleOwner, { response ->
+//           // response 상태에 따라 로직 나누기
+            when (response) {
+                is NetworkResult.Success -> {
+                    hideShimmerEffect()
+                    response.data?.let {
+                        mAdapter.setData(it)
+                    }
+                }
+                is NetworkResult.Error -> {
+                    hideShimmerEffect()
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                is NetworkResult.Loading -> {
+                    showShimmerEffect()
+                }
+
+            }
+        })
+    }
+
+    private fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+        queries["number"] = "50"
+        queries["apikey"] = API_KEY
+        queries["type"] = "snack"
+        queries["diet"] = "vegan"
+        queries["addRecipeInformation"] = "true"
+        queries["fillIngredients"] = "true"
+
+        return queries
 
     }
 
+    private fun setupRecyclerView() {
+        mView.recyclerview.adapter = mAdapter
+        mView.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun showShimmerEffect() {
+
+        //showShimmer 이란 리스트화면에서 데이터를 로딩하는 동안 흐르는 애니매이션
+        mView.recyclerview.showShimmer()
+    }
+
+    private fun hideShimmerEffect() {
+        mView.recyclerview.hideShimmer()
+    }
 
 }
