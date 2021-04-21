4e48b9b9bc3f630b896f0ca588fc25d6c8c9c293
KyungMin <2004ckck@naver.com>
2021-04-21 23:44:35 +0900

    [CREATE] query 값

---
+++ app/src/main/java/com/example/foody/util/Constrants.kt
@@ -0,0 +1,15 @@
+package com.example.foody.util
+
+class Constrants {
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "67cc03db07e946f59d11ce77f86aafec"
+
+        // API Query Keys
+        const val QUERY_NUMBER="number"
+        const val QUERY_API_KEY= API_KEY
+        const val QUERY_TYPE="type"
+        const val QUERY_ADD_RECIPE_INFORMATION="addRecipeInformation"
+        const val QUERY_fill_ingredients="fillIngredients"
+    }
+}
