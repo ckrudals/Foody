6ca02fc5cbca0ca1358e37d7dc8c959219c3a991
KyungMin <2004ckck@naver.com>
2021-04-26 19:37:48 +0900

    [CREATE] Repository

--- app/src/main/java/com/example/foody/data/Repository.kt
+++ app/src/main/java/com/example/foody/data/Repository.kt
@@ -3,10 +3,18 @@ package com.example.foody.data
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
 
+
+// ActivityRetainedScoped
+
+/***
+ * ApplicationComponent의 하위 컴포넌트느로써 Activity의 생명주기를 lifetiome으로 갖는다.
+ * Activity가 생성되는 시점에 생성되고, Activity가 파괴데는 시점에 파괴된다.
+ */
 @ActivityRetainedScoped
 class Repository @Inject constructor(
-    remoteDataSource: RemoteDataSource
-
+    remoteDataSource: RemoteDataSource,
+    localDataSource: LocalDataSource
 ) {
-    val remote=remoteDataSource
+    val remote = remoteDataSource
+    val local=localDataSource
 }
