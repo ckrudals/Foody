b67c1088d8044d02ad53f0111096ca89bda3b2bc
KyungMin <2004ckck@naver.com>
2021-04-26 19:37:04 +0900

    [CREATE] Room Database

---
+++ app/src/main/java/com/example/foody/data/database/RecipesDatabase.kt
@@ -0,0 +1,19 @@
+package com.example.foody.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.example.foody.RecipesTypeConverter
+
+@Database(
+    entities = [RecipesEntity::class],
+    // 스키마 변경할때마다 버전을 올려야한다.
+    version = 1,
+    exportSchema = false
+)
+
+@TypeConverters(RecipesTypeConverter::class)
+abstract class RecipesDatabase : RoomDatabase() {
+    abstract fun recipesDao(): RecipesDao
+}
