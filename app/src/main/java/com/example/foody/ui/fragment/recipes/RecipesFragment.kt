02955857423b6528f939830ba965a8cc810fb09b
KyungMin <2004ckck@naver.com>
2021-08-23 03:31:40 +0900

    [ADD] viewModel network

--- app/src/main/java/com/example/foody/ui/fragment/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragment/recipes/RecipesFragment.kt
@@ -9,24 +9,36 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foody.viewmodels.MainViewModel
 import com.example.foody.R
 import com.example.foody.adapters.RecipesAdapter
 import com.example.foody.databinding.FragmentRecipesBinding
+import com.example.foody.util.NetworkListener
 import com.example.foody.util.NetworkResult
+import com.example.foody.util.observeOnce
 import com.example.foody.viewmodels.RecipesViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
+
+    private val args by navArgs<RecipesFragmentArgs>()
+
     private var _binding: FragmentRecipesBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var a: String
+
     private lateinit var mainViewModel: MainViewModel
     private lateinit var recipesViewModel: RecipesViewModel
     private val mAdapter by lazy { RecipesAdapter() }
-    lateinit var mView: View
-
+    private lateinit var networkListener: NetworkListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
@@ -36,74 +48,98 @@ class RecipesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         _binding = FragmentRecipesBinding.inflate(inflater, container, false)
-
-
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
         setupRecyclerView()
-        readDataBase()
+
+        recipesViewModel.readBackOnline.observe(viewLifecycleOwner,{
+            recipesViewModel.backOnline=it
+        })
+        // suspend 로 보냈으니 비동기처리 코루틴으로 감싼다.
+        lifecycleScope.launch {
+            networkListener = NetworkListener()
+            networkListener.checkNetworkAvailability(requireContext())
+                .collect { status ->
+                    // 네트워크가 활성화 안되어있으면 false 활성화 되어있으면 true
+                    Log.d(TAG, "onCreateView: ${status.toString()} ")
+                    recipesViewModel.networkStatus = status
+                    recipesViewModel.showNetworkStauts()
+                    readDatabase()
+                }
+        }
+        binding.recipesFab.setOnClickListener {
+
+            // 인터넷 연결 되었나 ?
+            if (recipesViewModel.networkStatus) {
+                findNavController().navigate(R.id.action_recipesFragment_to_bottomSheetDialogFragment)
+
+            } else {
+                // 안되어있으면 Toast
+                recipesViewModel.showNetworkStauts()
+            }
+        }
 
         return binding.root
     }
 
-    private fun readDataBase() {
-        mainViewModel.readRecipes.observe(viewLifecycleOwner, { database ->
-            // 비어있지 않다면
-            if (database.isNotEmpty()) {
-
-                Log.d(TAG, "readDataBase: ")
-                mAdapter.setData(database[0].foodRecipe)
-
-            } else {
-
-                // 비어있으면
-                requestApiData()
-            }
-
-        })
+    private fun setupRecyclerView() {
+        binding.recyclerview.adapter = mAdapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
     }
 
+    private fun readDatabase() {
+        lifecycleScope.launch {
+            mainViewModel.readRecipes.observeOnce(viewLifecycleOwner, { database ->
+                if (database.isNotEmpty() && !args.backFromBottomSheet) {
+                    Log.d("RecipesFragment", "readDatabase called! ${database[0].foodRecipe}")
+                    mAdapter.setData(database[0].foodRecipe)
+                    hideShimmerEffect()
+                } else {
+                    requestApiData()
+                }
+            })
+        }
+    }
 
     private fun requestApiData() {
-
-        Log.d(TAG, "requestApiData: ")
+        Log.d("RecipesFragment", "requestApiData called!")
         mainViewModel.getRecipes(recipesViewModel.applyQueries())
         mainViewModel.recipesResponse.observe(viewLifecycleOwner, { response ->
-//           // response 상태에 따라 로직 나누기
             when (response) {
                 is NetworkResult.Success -> {
                     hideShimmerEffect()
-                    response.data?.let {
-                        mAdapter.setData(it)
-                    }
+                    response.data?.let { mAdapter.setData(it) }
                 }
                 is NetworkResult.Error -> {
                     hideShimmerEffect()
+                    loadDataFromCache()
                     Toast.makeText(
                         requireContext(),
                         response.message.toString(),
                         Toast.LENGTH_SHORT
                     ).show()
                 }
-
                 is NetworkResult.Loading -> {
                     showShimmerEffect()
                 }
-
             }
         })
     }
 
-
-    private fun setupRecyclerView() {
-        binding.recyclerview.adapter = mAdapter
-        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+    private fun loadDataFromCache() {
+        lifecycleScope.launch {
+            mainViewModel.readRecipes.observe(viewLifecycleOwner, { database ->
+                if (database.isNotEmpty()) {
+                    mAdapter.setData(database[0].foodRecipe)
+                }
+            })
+        }
     }
 
     private fun showShimmerEffect() {
-
-        //showShimmer 이란 리스트화면에서 데이터를 로딩하는 동안 흐르는 애니매이션
         binding.recyclerview.showShimmer()
     }
 
@@ -111,4 +147,8 @@ class RecipesFragment : Fragment() {
         binding.recyclerview.hideShimmer()
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
